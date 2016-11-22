e9693093f291c9a97d5447affc60cbb22ade5119
David Alvarado <davidealva@gmail.com>
2016-11-21 23:25:27 -0700

    Tied in links
    Tied in links and routes

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,13 @@
 class ProfileController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def step2
   end
 
   def step3
   end
+
+  def profile
+  end
+
 end
