9cbae1b54ad75830617a9e0bdd4bf6cdf255fdb8
David Alvarado <davidealva@gmail.com>
2016-11-21 23:50:04 -0700

    Add User object
    Added user object for views

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,12 +2,15 @@ class ProfileController < ApplicationController
   before_action :authenticate_user!
 
   def step2
+    @user = current_user
   end
 
   def step3
+    @user = current_user
   end
 
   def profile
+    @user = current_user
   end
 
 end
